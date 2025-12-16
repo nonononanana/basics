6622ea93c4b87da14244bd7c8b37bdd2d17593fd
Mona Lisa <haiyan.xu.vip@gmail.com>
2025-12-17 03:19:07 +0800

    bug

--- meanflow/data/__init__.py
+++ meanflow/data/__init__.py
@@ -1,20 +1,16 @@
 """
-Data module for Mean Flow
+Data module for Mean Flow - Denoising Only
 """
 
 from .rml_dataset import (
-    RML2016Dataset,
     RML2016DenoisingDataset,
-    get_rml_dataloaders,
     get_rml_denoising_dataloaders,
     EXPERIMENT_SETTINGS,
     ALL_MODULATIONS
 )
 
 __all__ = [
-    'RML2016Dataset',
     'RML2016DenoisingDataset',
-    'get_rml_dataloaders',
     'get_rml_denoising_dataloaders',
     'EXPERIMENT_SETTINGS',
     'ALL_MODULATIONS'
